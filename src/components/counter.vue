27f35778141cfdf4189adc53157fd841324a504a
ThomRib <tomasyolan@gmail.com>
2026-01-14 01:10:36 -0800

    exit counter.vue

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -2,13 +2,16 @@
     let count = 0;
 
     function increment(){
+        console.info(`Increment counter : ${count}`);
         count++;
+
+        document.getElementById("count").innerText =
     }
 </script>
 
 <template>
     <div>
-        <h1>Counter : {{ count }}</h1>
+        <h1 id="count">Counter : {{ count }}</h1>
         <button v-on:click="increment">Increment</button>
     </div>
 </template>
